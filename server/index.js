55db9ded24df4f094d84289ff7bc4f09c1e3624a
vasil bhisti <vbhisti66@gmail.com>
2024-12-18 22:06:52 +0530

    db connect

--- server/index.js
+++ server/index.js
@@ -4,9 +4,9 @@ const port = 3000;
 
 const app = express();
 
-const mongoURI = 'mongodb://user000:USERDB@cluster0.ekfoe.mongodb.net/';
-
-mongoose
+const mongoURI = 'mongodb+srv://user000:USERDB@cluster0.ekfoe.mongodb.net/invoice?retryWrites=true&w=majority';
+try {
+  mongoose
   .connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -17,6 +17,10 @@ mongoose
   .catch((err) => {
     console.error('MongoDB connection error:', err);
   });
+} catch(error) {
+console.log('ee', error)
+}
+
 
 app.listen(port, () => {
   console.log('running at port', port);
